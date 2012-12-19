0d21f7e476d5aef891f524d02125237d513f446c
Zach Moazeni <zach.moazeni@gmail.com>
2012-12-19 13:58:18 -0500

    Initial codebase will silly index page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herokusearch::Application.config.secret_token = '6ae7d385e2c9e307683dbf21f4ee04f017762ab17c21dd89245395d5089a931fbdf1fda0d0fd5e97633ce8ec91250f0d3abdac73a67b40d143cab33d35a42a46'
